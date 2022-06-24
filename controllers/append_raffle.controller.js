1635330f89d00ad56d3e7c865fa466875f109b97
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-06-24 15:38:12 +0800

    raffle

--- controllers/append_raffle.controller.js
+++ controllers/append_raffle.controller.js
@@ -7,15 +7,16 @@ const { createRaffle } = require('../modules/raffle.module')
 // api/products
 router.post('/append_raffle', async (req, res) => {
     try {
-        const props = req.body
-
         const assetsModel = await Assets()
         const transactionsModel = await Transactions()
 
+        var isCanAppend = true
+        var assetAppend
+
+        const props = req.body
+
         if (!props?.did || !props?.count)
             return res.status(400).json("Unauthorized")
-
-        var isCanAppend = true
 
         var fetchedLatestTransaction = await fetchLatestTransaction(props?.did)
 
@@ -28,15 +29,8 @@ router.post('/append_raffle', async (req, res) => {
         var fetchedAsset = await assetsModel.findOne({
             "data.did": props?.did,
         })
-        console.log(fetchedAsset?.id)
-        var fetchedRaffleLatestTransaction = await fetchLatestTransaction(fetchedAsset?.id)
-        console.log(fetchedRaffleLatestTransaction)
-
-
-        var assetAppend
 
         if (!fetchedAsset) {
-            console.log("creating")
             // if dont exist create
             assetAppend = await createRaffle({
                 asset: {
@@ -50,17 +44,19 @@ router.post('/append_raffle', async (req, res) => {
                 privateKey: user_wallet?.privateKey
             })
         } else {
-            console.log("updating")
-            // console.log(fetchedRaffleLatestTransaction)
-            console.log(fetchedRaffleLatestTransaction.metadata)
+            var fetchedRaffleLatestTransaction = await fetchLatestTransaction(fetchedAsset?.id)
+            if (!fetchedRaffleLatestTransaction) {
+                return res.status(400).json("Transaction does not exist")
+            }
+
             fetchedRaffleLatestTransaction.metadata.count = fetchedRaffleLatestTransaction.metadata.count + props?.count
+
             assetAppend = await updateSingleAsset({
                 txCreatedID: fetchedRaffleLatestTransaction?.id,
                 metadata: fetchedRaffleLatestTransaction.metadata,
                 publicKey: user_wallet.publicKey,
                 privateKey: user_wallet.privateKey,
             })
-            // console.log(assetAppend)
         }
 
         if (JSON.stringify(assetAppend) != JSON.stringify({})) {
